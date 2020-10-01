b7f6dadd4d2193be5000c2ada397afc3c56cf462
James-Muralles <james.muralles23@gmail.com>
2020-10-01 09:55:35 -0500

    added popups

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 
 import { listLogEntries } from "./API";
 
 const App = () => {
 	const [logEntries, setLogEntries] = useState([]);
+	const [showPopup, setShowPopup] = useState({});
 	const [viewport, setViewport] = useState({
 		width: "100vw",
 		height: "100vh",
@@ -29,21 +30,49 @@ const App = () => {
 			mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
 		>
 			{logEntries.map((entry) => (
-				<Marker
-					key={entry._id}
-					latitude={entry.latitude}
-					longitude={entry.longitude}
-					offsetLeft={-12}
-					offsetTop={-24}
-				>
-					<div>
-						<img
-							className="marker"
-							src="https://i.imgur.com/y0G5YTX.png"
-							alt="marker"
-						/>
-					</div>
-				</Marker>
+				<>
+					<Marker
+						key={entry._id}
+						latitude={entry.latitude}
+						longitude={entry.longitude}
+					>
+						<div
+							onClick={() =>
+								setShowPopup({
+									// ...showPopup,
+									[entry._id]: true,
+								})
+							}
+						>
+							<img
+								className="marker"
+								style={{
+									height: `${6 * viewport.zoom}px`,
+									width: `${6 * viewport.zoom}px`,
+								}}
+								src="https://i.imgur.com/y0G5YTX.png"
+								alt="marker"
+							/>
+						</div>
+					</Marker>
+					{showPopup[entry._id] ? (
+						<Popup
+							latitude={entry.latitude}
+							longitude={entry.longitude}
+							closeButton={true}
+							closeOnClick={false}
+							dynamicPosition={true}
+							onClose={() => setShowPopup({})}
+							anochor="top"
+						>
+							<div className="popup">
+								<h3>{entry.title}</h3>
+								<p>{entry.comments}</p>
+								<small>Visited on: {new Date(entry.visitDate).toLocaleDateString()}</small>
+							</div>
+						</Popup>
+					) : null}
+				</>
 			))}
 			;
 		</ReactMapGL>
