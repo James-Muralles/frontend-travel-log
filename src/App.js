0c912dbb57f51e814e9c3ef2424ed400776004a6
James-Muralles <james.muralles23@gmail.com>
2020-10-01 09:02:16 -0500

    added markers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { listLogEntries } from "./API";
+
+const App = () => {
+	const [logEntries, setLogEntries] = useState([]);
+	const [viewport, setViewport] = useState({
+		width: "100vw",
+		height: "100vh",
+		latitude: 37.6,
+		longitude: -95.665,
+		zoom: 3,
+	});
+
+	useEffect(() => {
+		(async () => {
+			const logEntries = await listLogEntries();
+			setLogEntries(logEntries);
+			console.log(logEntries);
+		})();
+	}, []);
+
+	return (
+		<ReactMapGL
+			{...viewport}
+			onViewportChange={setViewport}
+			mapStyle={"mapbox://styles/mapbox/streets-v11"}
+			mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+		>
+			{logEntries.map((entry) => (
+				<Marker
+					key={entry._id}
+					latitude={entry.latitude}
+					longitude={entry.longitude}
+					offsetLeft={-12}
+					offsetTop={-24}
+				>
+					<div>
+						<img
+							className="marker"
+							src="https://i.imgur.com/y0G5YTX.png"
+							alt="marker"
+						/>
+					</div>
+				</Marker>
+			))}
+			;
+		</ReactMapGL>
+	);
+};
 
 export default App;
